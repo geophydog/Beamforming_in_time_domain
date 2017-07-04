e48561ae2a7d41d3bdd6217950fba83a5adba332
geophydog <geophydogvon@gmail.com>
2017-07-04 15:44:44 +0800

    modified

--- beamforming.c
+++ beamforming.c
@@ -58,6 +58,12 @@ float coef( float *data1, float *data2, int npts ) {
 	return fabs(cof);
 }
 
+/*--------------------------------------------get sign of inputting parameter-------------------------------------------------------------------------------*/
+float sign( float x  ) {
+    if ( x >= 0. ) return 1.;
+    else return -1.;
+}
+
 /*----------------------------------------------------------------------------------------------------------------------------------------------------------*/
 #define pi 3.1415926  // The approximation value of PI
 #define g 111.195     // The value of transformation from degree to km in the Earth
@@ -66,14 +72,14 @@ float coef( float *data1, float *data2, int npts ) {
 /*-----------------------------------------------------MAIN PROGRAM-----------------------------------------------------------------------------------------*/
 int main( int argc, char *argv[] ) {
     float *data, slow_low, slow_high, slow_step, slow_scan, slow_x, slow_y, baz_scan, baz_step, grid, grid_step,\
-        shifttime, center_lon = 0., center_lat = 0., center_ele = 0., fre_low, fre_high, t1, t2,\
-        delta, dx, dy, *sum, time_start, time_end, time_used, **coordi, **amp, cof = 0., **tmp, cof_peak = 0., cof_low = 1.;
+        shifttime, center_lon = 0., center_lat = 0., center_ele = 0., fre_low, fre_high, t1, t2, delta, dx, dy, \
+        *sum, time_start, time_end, time_used, **coordi, **amp, cof = 0., **tmp, cof_peak = 0., cof_low = 1., Nth_root, tmp1, tmp2;
     int i, j, size = 256, count = 0, sac_npts, sta_index = 0, shift_index, begin_index, end_index, beam_npts, k = 0;
     char *ss;
     FILE *fin, *fout, *fp, *fbp;
     SACHEAD hd;
 
-    if ( argc != 11 ) {
+    if ( argc != 12 ) {
 	    fprintf(stderr,"***********************************************************************************************************************************\n");
         fprintf(stderr,"** Usage: beamforming <sacfile.lst> <t1> <t2> <fre_low> <fre_high> <slow_low> <slow_high> <slow_step> <baz_step> <output_file>   **\n");
         fprintf(stderr,"**       <t1>          Beginning time of inputing SAC files;                                                                     **\n");
@@ -85,6 +91,7 @@ int main( int argc, char *argv[] ) {
         fprintf(stderr,"**       <slow_high>   The high limitation of scaning horizontal slowness;                                                       **\n");
         fprintf(stderr,"**       <slow_step>   THe step length of scaning horizontal slowness;                                                           **\n");
         fprintf(stderr,"**       <baz_step>    The step length of scaning backazimuth;                                                                   **\n");
+        fprintf(stderr,"**       <Nth_root>    Nth_root slant stacking;                                                                                  **\n");
         fprintf(stderr,"**       <output_file> The file name of outputing result; Containing 3 columns Col1: baz  Col2: slowness  Col3: cross-coeffient  **\n");
         fprintf(stderr,"**                     <<<SPECIALLY, the unit of inputting slowness is sec/deg>>>;                                               **\n");
         fprintf(stderr,"**       ATTENTION!!! plot shell script will be saved in file \"plot.sh\", and just run command \"sh plot.sh!\";                     **\n");
@@ -102,7 +109,8 @@ int main( int argc, char *argv[] ) {
     slow_high = atof(argv[7]);
     slow_step = atof(argv[8]);
     baz_step = atof(argv[9]);
-    fout = fopen(argv[10],"w");
+    Nth_root = atof(argv[10]);
+    fout = fopen(argv[11],"w");
     fp = fopen("plot.sh", "w");
 
     time_start = clock();
@@ -163,8 +171,8 @@ int main( int argc, char *argv[] ) {
     slow_scan = slow_low;
 
     while ( slow_scan <= slow_high ) {
-        baz_scan = -50;
-        while( baz_scan <= 365 ) {
+        baz_scan = -10.;
+        while( baz_scan <= 370. ) {
             for ( i = 0; i < beam_npts; i++ ) {
                 sum[i] = 0., cof = 0.;
             }
@@ -172,40 +180,25 @@ int main( int argc, char *argv[] ) {
                 for ( j = 0; j < beam_npts; j ++ )
                     tmp[i][j] = 0.;
             for ( i = 0; i < count; i ++ ) {
-                slow_x = slow_scan*cos( (90 - baz_scan)/180.*pi );
-                slow_y = slow_scan*sin( (90 - baz_scan)/180.*pi );
+                slow_x = slow_scan*cos( (90. - baz_scan)/180.*pi );
+                slow_y = slow_scan*sin( (90. - baz_scan)/180.*pi );
 	            dx = center_lon - coordi[i][0]; dy = center_lat - coordi[i][1];
                 shifttime = slow_x*dx + slow_y*dy;
                 shift_index = (int)(shifttime/delta);
-                if ( (shift_index+begin_index) >= 0 && (shift_index+begin_index + beam_npts) < sac_npts ) {
-                    for ( j = 0; j < beam_npts; j ++ ) {
-                        sum[j] += amp[i][j+begin_index+shift_index]/count;
-                        tmp[i][j] = amp[i][j+begin_index+shift_index];
+                for ( j = 0; j < beam_npts; j ++ ) {
+                    if ( (begin_index + j + shift_index) >= 0 && (begin_index + j + shift_index) < sac_npts ) {
+                        tmp1 = sign(amp[i][begin_index+j+shift_index])*pow(fabs(amp[i][begin_index+j+shift_index]),1./Nth_root)/count;
+                        sum[j] += tmp1;
+                        tmp[i][j] = tmp1;
+                    }
+                    else {
+                        sum[j] += 0.; tmp[i][j] = 0.;
                     }
                 }
-                else if ( (shift_index+begin_index) < 0 ) {
-	                for (j = 0; j < beam_npts; j ++) {
-                        if ( (j+begin_index+shift_index) < 0 ) {
-                        	sum[j] += 0.; tmp[i][j] = 0.;
-                        }
-                    	else {
-                        sum[j] += amp[i][j+begin_index+shift_index];
-                        tmp[i][j] = amp[i][j+begin_index+shift_index];
-                    	}
-              	    }
-	            }
-                else if ( (shift_index+begin_index+beam_npts) >= sac_npts ) {
-                    for ( j = 0; j < beam_npts; j ++ ) {
-	                    if( (shift_index+begin_index+j) >= sac_npts ) {
-                        	sum[j] += 0.; tmp[i][j] = 0.;
-                    	}
-                    	else  {
-                        	sum[j] += amp[i][shift_index+begin_index+j];
-                        	tmp[i][j] = amp[i][j+begin_index+shift_index];
-                    	}
-                	}
-                }
-                else continue;
+            }
+            for ( j = 0; j < beam_npts; j ++ ) {
+                tmp2 = sign(sum[j])*pow(fabs(sum[j]),Nth_root);
+                sum[j] = tmp2;
             }
             for ( i = 0; i < count; i ++ ) {
                 cof += coef(sum, tmp[i], beam_npts)/count;
@@ -236,7 +229,7 @@ int main( int argc, char *argv[] ) {
 	}
 	fprintf(fp,"J=Pa6i\n");
 	fprintf(fp,"PS=%f-%f.ps\n", t1, t2); fprintf(fp,"PDF=%f-%f.pdf\n", t1, t2);
-	fprintf(fp,"awk '{print $1,$2,$3/%f}' %s > tmp.file\n", cof_peak, argv[10]);
+	fprintf(fp,"awk '{print $1,$2,$3/%f}' %s > tmp.file\n", cof_peak, argv[11]);
 	fprintf(fp,"gmt surface tmp.file -R$R1 -I%f/%f -Gtmp.grd\n", baz_step/10., slow_step/5.);
 	fprintf(fp,"gmt makecpt -Cjet -T%f/1/0.1 -Z >tmp.cpt\n", cof_low/cof_peak);
 	fprintf(fp,"gmt psxy -R$R2 -J$J -K -T>$PS\n");
